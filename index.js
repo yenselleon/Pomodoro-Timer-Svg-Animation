4e1cf4806695bbb938b77b83d952ec5393f8481e
yenselleon <76271225+yenselleon@users.noreply.github.com>
2021-05-21 23:15:51 -0400

    funcionalidad del break añadida

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {getParameter, setParametersInLocalStorage} from './settings.js'
     const secsClock = document.querySelector('.secs');
     const btnSendParameters = document.getElementById('sendParameters');
     const inputFocusTime = document.getElementById('focusTime');
+    const inputBreakTime = document.getElementById('brakTime');
     
     
     let h = 0;
@@ -16,10 +17,18 @@ import {getParameter, setParametersInLocalStorage} from './settings.js'
     
 window.onload = ()=> { btnEvents(); loadParameters(); };
 
-function loadParameters() {
 
+/*------------------------------------------*/
+/*--Cargar datos del LocalStorage--*/
+/*------------------------------------------*/
+function loadParameters() {
+    //Load input focus time value
     (JSON.parse(localStorage.getItem("parameters"))) ? inputFocusTime.value = Number(JSON.parse(localStorage.getItem("parameters")).focusTime)
                                                      : inputFocusTime.value = m;
+    //Load Input break time value
+    (JSON.parse(localStorage.getItem("parameters")) && Number(JSON.parse(localStorage.getItem("parameters")).breakTime !== "")) 
+                                                     ? inputBreakTime.value = Number(JSON.parse(localStorage.getItem("parameters")).breakTime)
+                                                     : inputBreakTime.value = 5;
 }
 
 function btnEvents() {
@@ -40,13 +49,23 @@ function startClock() {
     
     stopIntervalTimer = setInterval(()=> {
         
-        if (s === 0) {s = 60;  m--; s--;}
+        if (s === 0 && m === 0) { clearInterval(stopIntervalTimer); resetClock(); }
+        else if(s < 5 && m === 0) { console.log("animate"); s--; }
+        else if(s === 0) {s = 60;  m--; s--;}
         else if (s < 60) {s--}
         
         secsClock.innerHTML = `${String(s).padStart(2,"0")}`;
         minsClock.innerHTML = `${String(m).padStart(2,"0")}`;
     }, 1000)
 
+}
+
+/*------------------------------------------*/
+/*--START BREAK--*/
+/*------------------------------------------*/
+
+function startBreak() {
+    
 }
 
 /*------------------------------------------*/
@@ -63,8 +82,10 @@ function resetClock() {
 
 
     h = 0;
-    m = 25;
     s = 0;
+
+    (JSON.parse(localStorage.getItem("parameters"))) ? m = Number(JSON.parse(localStorage.getItem("parameters")).focusTime)
+                                                     : m = inputFocusTime.value;
 
     secsClock.innerHTML = `${String(s).padStart(2,"0")}`;
     minsClock.innerHTML = `${String(m).padStart(2,"0")}`;
